aa3960fb9173f930d0f8099e3a08684a517012d2
Ismail Ben Salah <hellgheast@gmail.com>
2015-05-04 13:54:30 +0200

    Mux codé

--- dcf_77_complete.ews/design/mux_dcf_test/rtl/rtl.vhd
+++ dcf_77_complete.ews/design/mux_dcf_test/rtl/rtl.vhd
@@ -22,8 +22,13 @@ signal test_vector std_logic_vector(7 downto 0):=x"00";
              
 begin
 
-test_vector <= test_vector(7 downto 1) & TEST;
-data_out <= 
+WITH adress_mux_dcf_test SELECT
+	 data_out <= test_vector(7 downto 1) & TEST 		WHEN "00",
+	 			 test_vector(7 downto 1) & start_acq 	WHEN "01",
+	 			 test_vector(7 downto 1) & buffer_full 	WHEN "10",
+	 			 data_dcf								WHEN "11",
+	 			 x"FF"			                        WHEN OTHERS;
+	 			 	
 
 
 end architecture rtl ; -- of mux_dcf_test
